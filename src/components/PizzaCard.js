1e0a27fd27c7a36a29055aa2ac562b3bf69f2694
Daniel Marton <instantbuddha@gmail.com>
2021-11-10 11:13:38 +0100

    minor css adjustments and cleaning for PizzaCard

--- src/components/PizzaCard.js
+++ src/components/PizzaCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function PizzaCard(props) {
-    const bootstrapStyle = "w-25 m-1 border border-secondary"
+    const bootstrapStyle = "w-25 m-1 border border-secondary container "
 
     const divCss = {
         backgroundImage: `url(${props.imageUrl})`,
@@ -16,19 +16,25 @@ export default function PizzaCard(props) {
         <div key={props.name}
             class={bootstrapStyle}
             style={divCss} >
-            <div >
-                <h1>{props.name}</h1>
-                <h2>{props.ingredients}</h2>
-                <h2>Full price: {props.fullPrice}</h2>
-                <FontAwesomeIcon icon="check-square" />
-                <FontAwesomeIcon icon="camera" />
-                <FontAwesomeIcon icon="cart-plus" />
-                <FontAwesomeIcon icon="shopping-cart" />
-                <FontAwesomeIcon icon="coffee" />
-
-            </div>
             <div>
-            <button >{props.fullPrice}</button> 
+            </div>    
+            <div class="row align-items-end">
+                <div class="col-9 " >
+                    <div>
+                        <h1>{props.name}</h1>
+                    </div>
+                    <div>
+                        <p >{props.ingredients}</p>
+                    </div>
+
+                </div>
+                <div class="col-3">
+                    <button >
+                        <FontAwesomeIcon icon="cart-plus" />
+                        {" $" + props.fullPrice}
+                    </button>
+                </div>
+
             </div>
 
         </div>
