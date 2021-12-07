f3a888c7e965c5e5f22650a9019a26ff73321027
Daniel Marton <instantbuddha@gmail.com>
2021-12-07 12:16:23 +0100

    add order details form structure

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -5,14 +5,85 @@ class OrderDetails extends Component {
         super(props)
     
         this.state = {
-             
+             clientInfo:{
+                 ClientName:"",
+                 ClientAddress:"",
+                 ClientEmail:"",
+                 ClientPhone:"",
+                 ClientNotes:""
+             },
+             tempClientInfo:{
+                ClientName:"",
+                ClientAddress:"",
+                ClientEmail:"",
+                ClientPhone:"",
+                ClientNotes:"",
+                agreed:false
+             }
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        console.log(event.target)
+        let copiedTempState = { ...this.state }
+
+        //switch
+
+        this.setState(copiedTempState)
     }
     
     render() {
         return (
             <div>
                 <h1>Order details</h1>
+                <form >
+                    <label>Your name: 
+                        <input
+                            type="text"
+                            name="name"
+                            onChange={this.handleChange}
+                        />    
+                    </label>
+                    <label >Your address: 
+                        <input
+                            type="text"
+                            name="address"
+                            onChange={this.handleChange}
+                        />    
+                    </label>
+                    <label>Your email:
+                        <input
+                            type="email"
+                            name="email"
+                            onChange={this.handleChange}
+                        />    
+                    </label>
+                    <label>Your phone number:
+                        <input
+                            type="tel"
+                            name="telephone"
+                            pattern="[0-9]{7}"
+                            onChange={this.handleChange}
+                        />    
+                    </label>
+                    <label>Your message to the pizzaboy:
+                        <input
+                            type="text"
+                            name="notes"
+                            onChange={this.handleChange}
+                        />    
+                    </label>
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="agreed"
+                            onChange={this.handleChange}
+                        />
+                        This is not a prank    
+                    </label>
+                    <input type="submit" />
+                </form>
             </div>
         )
     }
