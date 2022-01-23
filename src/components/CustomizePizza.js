d4b138f35c2ee5833aa324f6efd2d0abcead28b8
Daniel Marton <instantbuddha@gmail.com>
2022-01-23 16:15:59 +0100

    add CustomPizza screen

---
+++ src/components/CustomizePizza.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class CustomizePizza extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+    
+  render() {
+    return <div>
+        <h1>Customize pizza</h1>
+    </div>
+  }
+}
+
+export default CustomizePizza
